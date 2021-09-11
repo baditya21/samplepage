ef34794efcf83c32bdac15a39dfe16824f3aaa91
baditya21 <80754818+baditya21@users.noreply.github.com>
2021-09-11 16:50:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@
           slideIndex = 1
         }
         slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 3000); // Change image every 2 seconds
+        setTimeout(showSlides, 5000); // Change image every 2 seconds
       }
     
 })(jQuery);
