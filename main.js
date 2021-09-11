4ed80d8ac8c7194aee450f57ef5104ef8c736020
baditya21 <80754818+baditya21@users.noreply.github.com>
2021-09-11 20:19:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+(function ($) {
+    "use strict";
+    
+    // loader
+    var loader = function () {
+        setTimeout(function () {
+            if ($('#loader').length > 0) {
+                $('#loader').removeClass('show');
+            }
+        }, 1);
+    };
+    loader();
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "73px");
+        } else {
+            $('.nav-bar').removeClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "0");
+        }
+    });
+    
+})(jQuery);
+
+// slideshow
+
+var slide_index = 1;  
+displaySlides(slide_index);  
+
+function nextSlide(n) {  
+    displaySlides(slide_index += n);  
+}  
+
+function currentSlide(n) {  
+    displaySlides(slide_index = n);  
+}  
+
+function displaySlides(n) {  
+    var i;  
+    var slides = document.getElementsByClassName("showSlide");  
+    if (n > slides.length) { slide_index = 1 }  
+    if (n < 1) { slide_index = slides.length }  
+    for (i = 0; i < slides.length; i++) {  
+        slides[i].style.display = "none";  
+    } 
+    slides[slide_index - 1].style.display = "block";  
+    automate();  
+}  
+function automate() {
+    setTimeout(() => {  nextSlide(1) }, 5000);
+  }
